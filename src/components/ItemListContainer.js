f532f19e1a895bd72c5836f1650269e5d6adc943
paupuentes919 <paupuentes919@gmail.com>
2022-09-16 23:22:10 -0300

    NavBar responsive

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+const ItemListContainer = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+export default ItemListContainer
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,33 @@
 import CartWidget from './CartWidget'
+import pacman from './pacman.png'
 
 const NavBar = () => {
     return(
-        <div>
-            <nav className="PackMan-NavBar">
-                <div className="order-items-NavBar">
-                    <div className="navbar-items">
-                        <h3 className='title-navbar'>PacFoodie Company</h3>
-                        <img className="PackMan-Photo" src={require('../components/pacman.png')} />
-                        <button>
-                            <h5 className='title-navbar-2'>Home</h5>
-                        </button>
-                        <button>
-                            <h5 className='title-navbar-2'>Menu</h5>
-                        </button>
-                        <button>
-                            <h5 className='title-navbar-2'>Nosotros</h5>
-                        </button>
-                        <button>
-                            <h5 className='title-navbar-2'>Contacto</h5>
-                        </button>
-                    </div>
-                    <CartWidget/>
+        <div className="navbar bg-base-100 navbar-background">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Menu</a></li>
+                    <li><a>Sucursales</a></li>
+                    <li><a>Contacto</a></li>
+                    </ul>
                 </div>
-            </nav>  
+                <a className="PackMan-NavBar">PacFoodie Company</a>
+                <img className="PackMan-Photo" src={pacman}/>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0 PackMan-NavBar-2">
+                    <li><a>Menu</a></li>
+                    <li><a>Sucursales</a></li>
+                    <li><a>Contacto</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <CartWidget/>
+            </div>
         </div>
     )
 }
