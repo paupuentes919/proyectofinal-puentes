010fe6c5a2be04d14346134ad24f76a5618cb999
paupuentes919 <paupuentes919@gmail.com>
2022-09-16 22:42:29 -0300

    NavBar

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+        <div>
+            <button className="btn-login">
+                <img className="cart-shop" src={require('../components/carrito.png')} />
+            </button>
+        </div>
+    )
+}
+export default CartWidget
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,29 @@
+import CartWidget from './CartWidget'
+
 const NavBar = () => {
     return(
         <div>
             <nav className="PackMan-NavBar">
-                <div className="navbar-items">
-                    <h3>PacFoodie Company</h3>
-                    <img className="PackMan-Photo" src={require('../components/pacman.png')} />
+                <div className="order-items-NavBar">
+                    <div className="navbar-items">
+                        <h3 className='title-navbar'>PacFoodie Company</h3>
+                        <img className="PackMan-Photo" src={require('../components/pacman.png')} />
+                        <button>
+                            <h5 className='title-navbar-2'>Home</h5>
+                        </button>
+                        <button>
+                            <h5 className='title-navbar-2'>Menu</h5>
+                        </button>
+                        <button>
+                            <h5 className='title-navbar-2'>Nosotros</h5>
+                        </button>
+                        <button>
+                            <h5 className='title-navbar-2'>Contacto</h5>
+                        </button>
+                    </div>
+                    <CartWidget/>
                 </div>
-            
-            </nav>
-            
+            </nav>  
         </div>
     )
 }
