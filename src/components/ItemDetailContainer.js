29b7466391e6004a01f034b81be2a636f6fa3015
paupuentes919 <paupuentes919@gmail.com>
2022-09-30 17:18:37 -0300

    CSS Btns styles

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -2,6 +2,10 @@ import { useEffect, useState, React } from 'react'
 import { useParams } from 'react-router-dom'
 import { products } from './data/Products'
 import ItemCount from './ItemCount'
+import pacmanfantasmas from '../images/pacmanfantasmitas.jpg'
+import {Link} from "react-router-dom"
+import cross from "../images/cross.png"
+import circle from "../images/circle.png"
 
 const ItemDetailContainer = () => {
     const { id: itemId } = useParams()
@@ -22,6 +26,19 @@ const ItemDetailContainer = () => {
         })
     }
 
+    const Loader = () => {
+        let circleCommonClasses = 'h-2.5 w-2.5 bg-current rounded-full';
+   
+        return (
+       <div className='flex'>
+            <div className={`${circleCommonClasses} mr-1`}></div>
+            <div className={`${circleCommonClasses} mr-1`}></div>
+            <div className={`${circleCommonClasses}`}></div>
+       </div>
+        );
+   };
+
+
     const Item = ({itemProduct}) => {
         return (
             <div className="image-text-container">
@@ -31,7 +48,20 @@ const ItemDetailContainer = () => {
                     <div className="item-description-text">{itemProduct.description}</div>
                     <div className="item-description-text">Price: $ {itemProduct.price}</div>
                     <div className="item-description-text">Stock: {itemProduct.stock}</div>
-                    <ItemCount/>
+                    <div class="counter-ghosts">
+                        <ItemCount/>
+                    </div>
+                    <div className="counter-ghosts">
+                    <button className="btn-add">
+                        Agregar a Mi Compra
+                        <img className="circle" src={circle}/>
+                    </button>
+                    <img className="ghosts-pacman-image" src={pacmanfantasmas} /> 
+                    <Link to='/' className="btn-cancel">                    
+                        Volver al Home
+                        <img className="cross" src={cross}/>
+                    </Link>
+                    </div>
                 </div> 
             </div>
         )
